9ddf3fdedeabc21444d689ff42e3579ee4843fcc
Ricardo De Mauro <ricardo.demauro@coxautoinc.com>
2018-08-09 17:22:55 -0300

    adding initial frontend

---
+++ frontend/SimpleCMS/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleCMS.Models;
+using System.Collections.Generic;
+
+namespace SimpleCMS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Document> documentsMock = new List<Document>()
+            {
+                new Document
+                {
+                    Description = "Sample document"
+                }
+            };
+            return View(documentsMock);
+        }
+    }
+}
